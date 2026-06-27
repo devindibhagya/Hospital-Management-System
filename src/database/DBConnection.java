b03a73ff4c3db1f442c0865e8283610097f3c803
Bhagya Devindi <bhagyadevi709@gmail.com>
2026-06-27 18:38:08 +0530

    Update DBConnection.java

--- src/database/DBConnection.java
+++ src/database/DBConnection.java
@@ -5,8 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-    // ⚠️ ඔයාගේ MySQL Port එක 3306 නෙවෙයි වෙන එකක් නම් (XAMPP එකේ පේනවා) ඒක වෙනස් කරන්න.
-    // ⚠️ ඔයා MySQL වලට Password එකක් දාලා තියෙනවා නම් "" වෙනුවට ඒක ලියන්න.
+    
     private static final String URL = "jdbc:mysql://localhost:3306/hospital_db";
     private static final String USER = "root";
     private static final String PASSWORD = ""; 
@@ -18,11 +17,11 @@ public class DBConnection {
             try {
                 Class.forName("com.mysql.cj.jdbc.Driver");
                 connection = DriverManager.getConnection(URL, USER, PASSWORD);
-                System.out.println("Database එක සමඟ සාර්ථකව සම්බන්ධ විය!");
+                System.out.println("Connected to the database successfully!");
             } catch (ClassNotFoundException e) {
-                System.out.println("MySQL Driver එක සොයාගත නොහැක: " + e.getMessage());
+                System.out.println("MySQL Driver cannot found " + e.getMessage());
             } catch (SQLException e) {
-                System.out.println("Database සම්බන්ධතාවය අසාර්ථකයි: " + e.getMessage());
+                System.out.println("Database connection failed: " + e.getMessage());
             }
         }
         return connection;
